d23d7856564eaa39563812301c7102242febd51c
Prabhjyot <knowprabhjyot@gmail.com>
2022-08-24 10:10:57 -0700

    final changes

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const [name, setName ] = useState("");
+
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    console.log('working');
+   
+    try {
+      const response = await axios.post('http://localhost:8080/user/register', {
+        name
+      })
+
+      console.log(response, "response");
+    } catch (error){
+      console.log(error, "i got this error");
+    }
+    
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submitForm}>
+        <input value={name} placeholder="Enter Name" onChange={(event) => setName(event.target.value) } />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
